fc9568cf8aac75ee69ad3537620882aee6aeb103
ecstasy-cypher <abiodunsulaimon08@gmail.com>
2025-02-20 23:17:24 +0100

    alx-react-app

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
     <Header />
     <MainContent />
     <Footer />
-    <UserProfile name="Dada" age="25" bio="Hello everyone, I am learning React at ALX!" />
+    <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />

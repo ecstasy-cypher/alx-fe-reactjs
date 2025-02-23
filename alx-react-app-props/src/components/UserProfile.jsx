cc6159998a02a1330d3907410eafca26173f46b6
ecstasy-cypher <abiodunsulaimon08@gmail.com>
2025-02-23 13:32:48 +0100

    alx-react-app-props

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,12 +1,16 @@
-// src/components/UserProfile.jsx
-function UserProfile(props) {
+// src/components/UserDetails.jsx
+import { useContext } from 'react';
+import UserContext from '../UserContext';
+
+function UserDetails() {
+  const userData = useContext(UserContext);
+
   return (
-    <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
-      <h2 style={{ color: 'blue', fontSize: '24px' }}>{props.name}</h2>
-      <p style={{ marginBottom: '5px' }}>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
-      <p style={{ fontStyle: 'italic' }}>Bio: {props.bio}</p>
+    <div>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
     </div>
   );
 }
 
-export default UserProfile;
+export default UserDetails;
